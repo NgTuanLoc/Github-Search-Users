3839a80c8cff6dba7ccdb20e269b5b5c9dc5df7e
NgTuanLoc <18521011@gm.uit.edu.vn>
2022-07-11 13:50:26 +0700

    add Login Page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,37 @@
+import styled from 'styled-components';
+import LoginImage from '../images/login-img.svg';
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+	return (
+		<Container>
+			<div className='container'>
+				<img src={LoginImage} alt='Login' />
+				<h1>Github User</h1>
+				<button className='btn' type='button'>
+					Login
+				</button>
+			</div>
+		</Container>
+	);
+};
 
-export default Login
+const Container = styled.section`
+	min-height: 100vh;
+	place-items: center;
+	display: grid;
+
+	.container {
+		width: 90vw;
+		max-width: 600px;
+		text-align: center;
+	}
+
+	img {
+		margin-bottom: 2rem;
+	}
+	h1 {
+		margin-bottom: 1rem;
+	}
+`;
+
+export default Login;
