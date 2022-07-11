390669560420ab7dfd437d9702dbc92167161deb
NgTuanLoc <18521011@gm.uit.edu.vn>
2022-07-11 19:49:41 +0700

    add Chart Component

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,8 +1,73 @@
+import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+
+import { Pie, Column, Doughnut, Bar } from '../components/Charts';
 
 const Repos = () => {
-  return (
-    <div>Repos</div>
-  )
-}
+	const { repos } = useSelector((store) => store.githubUser);
 
-export default Repos
+	const languages = repos.reduce((total, item) => {
+		const { language, stargazers_count } = item;
+		if (!language) return total;
+		if (!total[language]) {
+			total[language] = { label: language, value: 1, stars: stargazers_count };
+		} else {
+			total[language] = {
+				...total[language],
+				value: total[language].value + 1,
+				stars: total[language].stars + stargazers_count,
+			};
+		}
+		return total;
+	}, {});
+
+	const mostUsed = Object.values(languages)
+		.sort((a, b) => {
+			return b.value - a.value;
+		})
+		.slice(0, 5);
+
+	// most stars per language
+
+	const mostPopular = Object.values(languages)
+		.sort((a, b) => {
+			return b.stars - a.stars;
+		})
+		.map((item) => {
+			return { ...item, value: item.stars };
+		})
+		.slice(0, 5);
+
+	// stars, forks
+
+	let { stars, forks } = repos.reduce(
+		(total, item) => {
+			const { stargazers_count, name, forks } = item;
+			total.stars[stargazers_count] = { label: name, value: stargazers_count };
+			total.forks[forks] = { label: name, value: forks };
+			return total;
+		},
+		{
+			stars: {},
+			forks: {},
+		}
+	);
+
+	stars = Object.values(stars).slice(-5).reverse();
+	forks = Object.values(forks).slice(-5).reverse();
+
+	return (
+		<section className='section'>
+			<Container className='section-center'>
+				<Pie data={mostUsed} />
+				<Column data={stars} />
+				<Doughnut data={mostPopular} />
+				<Bar data={forks} />
+			</Container>
+		</section>
+	);
+};
+
+const Container = styled.div``;
+
+export default Repos;
