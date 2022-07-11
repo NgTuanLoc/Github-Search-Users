f97731b4f0120d13adc63e60bce0733f005dfc5b
NgTuanLoc <18521011@gm.uit.edu.vn>
2022-07-11 14:24:56 +0700

    User Info component completed

--- src/features/githubUserSlice.js
+++ src/features/githubUserSlice.js
@@ -1,15 +1,23 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+import mockUser from '../data/mockUser';
+import mockRepos from '../data/mockRepos';
+import mockFollowers from '../data/mockFollowers';
+
 const initialState = {
-  test: "Hello"
-}
+	githubUsers: mockUser,
+	repos: mockRepos,
+	followers: mockFollowers,
+};
 
 const githubUserSlice = createSlice({
-  name: "githubUser", initialState, reducers: {
-    getUser: () => {
-      console.log("Hello")
-    }
-  }
-})
+	name: 'githubUser',
+	initialState,
+	reducers: {
+		getUser: () => {
+			console.log('Hello');
+		},
+	},
+});
 
-export default githubUserSlice.reducer
+export default githubUserSlice.reducer;
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,18 @@
-const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+import styled from 'styled-components';
+import { Info, Repose, User, Search, Navbar, Repos } from '../components';
 
-export default Dashboard
+const Dashboard = () => {
+	return (
+		<Container>
+			<Navbar />
+			<Search />
+			<Info />
+			<User />
+			<Repos />
+		</Container>
+	);
+};
+
+const Container = styled.main``;
+
+export default Dashboard;
